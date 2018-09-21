92b241d5246343ad477d8991b3858c87fa79f91e
jaraivan <ivanjaratamargo@gmail.com>
2018-09-21 07:21:54 -0300

    agrego el seguirCamaraCtrl y hago que siga al jugador

---
+++ Assets/CamaraSeguirCtrl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamaraSeguirCtrl : MonoBehaviour {
+
+	public GameObject jugador;
+	private Rigidbody2D rb2dJugador;
+	// Use this for initialization
+	void Start () {
+		rb2dJugador = jugador.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate(){
+		Vector3 posicionJugador = rb2dJugador.position;
+		transform.position = new Vector3(posicionJugador.x,posicionJugador.y,transform.position.z);
+	}
+}
