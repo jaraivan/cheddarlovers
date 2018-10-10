fcc39d9e6c54ea0f9b3778f9630f13684b3da310
Gabriela Prystupiuk <gabrielaprys@gmail.com>
2018-10-09 23:45:21 -0300

    empiezo a agregar la funcionalidad y graficas del baul en la casa de lucas

---
+++ Assets/BaulCrtl.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public enum EstadoDelBaul{Abierto, Cerrado};
+public class BaulCrtl : MonoBehaviour {
+
+	private EstadoDelBaul estadoDelBaul = EstadoDelBaul.Cerrado;
+
+	private BoxCollider2D col;
+
+	private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		col = GetComponent<BoxCollider2D>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerStay2D(Collider2D col){
+		if(col.gameObject.tag == "Player" && Input.GetKeyDown(KeyCode.Space)) {
+            AbrirBaul();
+		}
+}
+    
+	void AbrirBaul() {
+		
+	}
+
+}
