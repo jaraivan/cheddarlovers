dc43b5c4b28679878154df95a829e46a6234bf53
jaraivan <ivanjaratamargo@gmail.com>
2018-10-13 22:13:55 -0300

    arreglo el script de donia paulina y un poco de refactor en el mismo controller

--- Assets/Scripts/DoniaPaulinaCtlr.cs
+++ Assets/Scripts/DoniaPaulinaCtlr.cs
@@ -8,6 +8,9 @@ public class DoniaPaulinaCtlr : MonoBehaviour {
 	public GameObject lucas;
 	public GameObject conversacion;
 	private BoxCollider2D bc2d;
+
+	private Queue<string> conversacionActual;
+	private Mision misionActual;
 	
 	// Use this for initialization
 	private int indiceDialogo = 0;
@@ -22,58 +25,18 @@ public class DoniaPaulinaCtlr : MonoBehaviour {
 	}
 
 	void OnTriggerStay2D(Collider2D col){
-		Mision mision = col.gameObject.GetComponent<JugadorCtrl>().GetAdministradorDeMisiones().GetMision();
-		Queue<string> queueDeTextoARecorrer = new Queue<string>(mision.GetDialogo().GetLineasDeTexto());
-		
-
-
-		if(col.gameObject.tag == "Player" && col.gameObject.GetComponent<JugadorCtrl>().GetEstadoDelJugador() == EstadoDelJugador.Jugando){
-			if(Input.GetKeyDown(KeyCode.Space)){
-				col.gameObject.GetComponent<JugadorCtrl>().IniciarConversacion();
-				ActivarConversacion();
-				conversacion.GetComponentInChildren<Text>().text = queueDeTextoARecorrer.Dequeue();
-			}
-		}
-		if(col.gameObject.tag == "Player" && col.gameObject.GetComponent<JugadorCtrl>().GetEstadoDelJugador() == EstadoDelJugador.Conversando){
-			if(Input.GetKeyDown(KeyCode.Space)){
-				
-				if(queueDeTextoARecorrer.Count ==0){
-					this.DesactivarConversacion(mision);
-					}else{
-
-					conversacion.GetComponentInChildren<Text>().text = queueDeTextoARecorrer.Dequeue();
-					}
-			}
-		}
+		this.ComportamientoJugando(col);
+		this.ComportamientoConversando(col);
 	}
 
 	void OnTriggerEnter2D(Collider2D col){
-		Mision mision = col.gameObject.GetComponent<JugadorCtrl>().GetAdministradorDeMisiones().GetMision();
-		Queue<string> queueDeTextoARecorrer = new Queue<string>(mision.GetDialogo().GetLineasDeTexto());
-		
-
-
-		if(col.gameObject.tag == "Player" && col.gameObject.GetComponent<JugadorCtrl>().GetEstadoDelJugador() == EstadoDelJugador.Jugando){
-			if(Input.GetKeyDown(KeyCode.Space)){
-				col.gameObject.GetComponent<JugadorCtrl>().IniciarConversacion();
-				ActivarConversacion();
-				conversacion.GetComponentInChildren<Text>().text = queueDeTextoARecorrer.Dequeue();
-			}
-		}
-		if(col.gameObject.tag == "Player" && col.gameObject.GetComponent<JugadorCtrl>().GetEstadoDelJugador() == EstadoDelJugador.Conversando){
-			if(Input.GetKeyDown(KeyCode.Space)){
-				
-				if(queueDeTextoARecorrer.Count ==0){
-					this.DesactivarConversacion(mision);
-					}else{
-
-					conversacion.GetComponentInChildren<Text>().text = queueDeTextoARecorrer.Dequeue();
-					}
-			}
-		}
+		this.ComportamientoJugando(col);
+		this.ComportamientoConversando(col);
 	}
 	void ActivarConversacion(){
 		conversacion.SetActive(true);
+		this.misionActual = GameObject.FindGameObjectWithTag("Player").GetComponent<JugadorCtrl>().GetAdministradorDeMisiones().GetMision();
+		this.conversacionActual = CopiarConversacion(this.misionActual.GetDialogo().GetLineasDeTexto());
 	}
 
 	void DesactivarConversacion(Mision mision){
@@ -84,5 +47,38 @@ public class DoniaPaulinaCtlr : MonoBehaviour {
 		conversacion.SetActive(false);
 	}
 
+	Queue<string> CopiarConversacion(Queue<string> queueQueMePasan){
+		Queue<string> nuevaQueue = new Queue<string>();
+		
+		foreach(string e in queueQueMePasan){
+			nuevaQueue.Enqueue(e);
+		}
+		
+		return nuevaQueue;
+	}
+
+	void ComportamientoJugando(Collider2D col){
+		if(col.gameObject.tag == "Player" && col.gameObject.GetComponent<JugadorCtrl>().GetEstadoDelJugador() == EstadoDelJugador.Jugando){
+			if(Input.GetKeyDown(KeyCode.Space)){
+				col.gameObject.GetComponent<JugadorCtrl>().IniciarConversacion();
+				ActivarConversacion();
+				conversacion.GetComponentInChildren<Text>().text = this.conversacionActual.Dequeue();
+			}
+		}
+	}
+
+	void ComportamientoConversando(Collider2D col){
+		if(col.gameObject.tag == "Player" && col.gameObject.GetComponent<JugadorCtrl>().GetEstadoDelJugador() == EstadoDelJugador.Conversando){
+			if(Input.GetKeyDown(KeyCode.Space)){
+				
+				if(this.conversacionActual.Count ==0){
+					this.DesactivarConversacion(this.misionActual);
+					}else{
+
+					conversacion.GetComponentInChildren<Text>().text = this.conversacionActual.Dequeue();
+					}
+			}
+		}
+	}
 
 }
