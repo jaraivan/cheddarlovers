764bf7da6e3a91bce28de24ba478d993734e37e6
gaby <gabrielaprys@gmail.com>
2018-11-23 21:10:51 -0300

    agrego el video de la intro y su scrip

---
+++ Assets/Scripts/IntroCrtl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class IntroCrtl : MonoBehaviour {
+
+	private VideoPlayer intro;
+
+
+	// Use this for initialization
+	void Awake () {
+		intro = GetComponent<VideoPlayer>();
+	}
+
+	void Start() {
+		intro.loopPointReached += TerminarIntro;
+	}
+	
+ void TerminarIntro(VideoPlayer vp) {
+	SceneManager.LoadScene(2);
+}
+	
+}
