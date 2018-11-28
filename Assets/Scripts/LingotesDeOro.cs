68c09445571d305e9aa32aa3f86d46ca8eb429af
gaby <gabrielaprys@gmail.com>
2018-11-27 23:09:52 -0300

    agrego scrip al oro de la mina

---
+++ Assets/Scripts/LingotesDeOro.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LingotesDeOro : MonoBehaviour {
+
+	public int valor = 3000;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D col){
+		if(col.gameObject.tag == "Player" && Input.GetKeyDown(KeyCode.Space)) {
+			GameObject.FindGameObjectWithTag("Player").GetComponent<JugadorCtrl>().AgregarOro(valor);
+            Destroy(gameObject);
+		}
+}
+}
