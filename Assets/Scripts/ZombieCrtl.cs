fe9faec26dd701f3e7ca88ace7ae8894f028e35b
ivanjara <ivanjaratamargo@gmail.com>
2018-11-28 04:10:51 -0300

    modifique el ataque de los zombies, ya resta vida

--- Assets/Scripts/ZombieCrtl.cs
+++ Assets/Scripts/ZombieCrtl.cs
@@ -31,6 +31,12 @@ public class ZombieCrtl : MonoBehaviour {
         rb2d = GetComponent<Rigidbody2D>();
     }
 
+    void OnTriggerEnter2D(Collider2D col){
+        if(col.gameObject.tag == "Player"){
+            col.gameObject.GetComponent<JugadorCtrl>().RestarVida(5);
+        }
+    }
+
     void Update () {
 
         // Por defecto nuestro target siempre será nuestra posición inicial
